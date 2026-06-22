49b9eccf0fdc1a92ab23238d6b521232940ee536
Wesley Marques <wesleymarquesdc@gmail.com>
2026-06-22 20:43:17 -0300

    test: add create task service unit test

--- backend/tests/unit/test_task_service.py
+++ backend/tests/unit/test_task_service.py
@@ -15,3 +15,26 @@ from tracko.domain.task.task_services import (
     task_service_read_many,
     task_service_read_one,
 )
+
+
+def test_task_service_create():
+    uow = MagicMock()
+
+    uow.tasks.add.side_effect = lambda task: task  # simula salvar e retornar
+
+    user = MagicMock()
+    user.id = uuid4()
+
+    data = TaskCreateSchema(
+        title='Test Task',
+        description='desc',
+        start_date=None,
+        end_date=None,
+    )
+
+    result = task_service_create(uow=uow, current_user=user, data=data)
+
+    uow.tasks.add.assert_called_once()
+
+    assert result.title == 'Test Task'
+    assert result.description == 'desc'
