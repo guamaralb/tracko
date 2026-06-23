aaadf671f1fecaaec3481510d92dfdde438d48e6
Wesley Marques <wesleymarquesdc@gmail.com>
2026-06-22 21:13:03 -0300

    test: add create user service unit test

--- backend/tests/unit/test_user_service.py
+++ backend/tests/unit/test_user_service.py
@@ -15,3 +15,21 @@ from tracko.domain.user.user_services import (
     user_service_read_many,
     user_service_read_one,
 )
+
+def test_user_service_create():
+    uow = MagicMock()
+
+    uow.users.add.side_effect = lambda user: user
+
+    data = UserCreateSchema(
+        email='test@test.com', password='123456', name='Test User'
+    )
+
+    current_user = MagicMock()
+
+    result = user_service_create(uow=uow, current_user=current_user, data=data)
+
+    uow.users.add.assert_called_once()
+
+    assert result.email == 'test@test.com'
+    assert result.name == 'Test User'
